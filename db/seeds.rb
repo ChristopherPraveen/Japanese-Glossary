5d5e3193f6646174eaff7b575ee6d590ce7c2a9b
Christopher Praveen <christopherpraveen@gmail.com>
2013-01-26 16:21:39 +0530

    Seeding Data 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+# encoding: utf-8
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 glossary = Glossary.create(glossary: "FFM Insurance Terms")
 
 Word.create(english:"memory decline debilitating memory loss",romaji:"",japanese:"記憶力の低下", glossary: glossary)
